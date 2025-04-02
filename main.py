7e40e4644f97f4637cf616d50b86b2e6e538e3ca
BeauHemeryck <HemeryckBeau1999@gmail.com>
2025-04-02 16:11:38 +0200

    Made bookbot dynamic with book input

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from stats import get_amount_of_word, get_book_text, get_amount_of_char, sort_dictionary
+from sys import *
 def main():
-    frank_path = "./books/frankenstein.txt"
-    frank = get_book_text(frank_path)
-    num_words = get_amount_of_word(frank)
-    char_count = sort_dictionary(get_amount_of_char(get_book_text("./books/frankenstein.txt")))
-    print(f"============ BOOKBOT ============\nAnalyzing book found at {frank_path[2:]}\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------")
-    for char in char_count:
-        if char.isalpha():
-            print(f"{char}: {char_count[char]}")
+    input = argv
+    if len(input) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+    else:
+        frank_path = input[1]
+        frank = get_book_text(frank_path)
+        num_words = get_amount_of_word(frank)
+        char_count = sort_dictionary(get_amount_of_char(get_book_text(frank_path)))
+        print(f"============ BOOKBOT ============\nAnalyzing book found at {frank_path[2:]}\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------")
+        for char in char_count:
+            if char.isalpha():
+                print(f"{char}: {char_count[char]}")
 
 main()
